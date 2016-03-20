cbeae25957f8cf01378b1ab0af1f44ff72154663
lishuhuakai <lishuhuakai@qq.com>
2016-03-21 00:04:09 +0800

    2016-03-21

--- 04/base/helper.rkt
+++ 04/base/helper.rkt
@@ -1,9 +1,9 @@
 #lang eopl
 (provide (all-defined-out))
 ;; print 
-;; 自定义的辅助函数,输出打印信息
 (define (print arg1 arg2)
   (begin
     (newline)
     (display arg1)
-    (display arg2)))
+    (display arg2)
+    (newline)))
