7046d51e6debaaa8612b016d78abf42942a1483e
lishuhuakai <lishuhuakai@gmial.com>
2016-03-22 22:07:09 +0800

    2016-03-22

---
+++ 05/08.rkt
@@ -0,0 +1,275 @@
+#lang eopl
+(require "./base/helper.rkt")
+;; Add multiargument procedures to this interpreter.
+(define identifier? symbol?)
+;;;;;;;;;;;;;;;;;;;;;;;;;continuation;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define-datatype continuation continuation?
+  (end-cont)
+  (zero1-cont
+   (cont continuation?))
+  (let-exp-cont
+   (var identifier?)
+   (body expression?)
+   (env environment?)
+   (cont continuation?))
+  (if-test-cont
+   (exp2 expression?)
+   (exp3 expression?)
+   (env environment?)
+   (cont continuation?))
+  (diff1-cont
+   (exp2 expression?)
+   (env environment?)
+   (cont continuation?))
+  (diff2-cont
+   (val1 expval?)
+   (cont continuation?))
+  
+  (rator-cont
+   (rands (list-of expression?))
+   (env environment?)
+   (cont continuation?))
+  
+  (rand1-cont
+   (rands (list-of expression?))
+   (env environment?)
+   (cont continuation?)
+   (body expval?)
+   (acc list?))
+  (rand-cont
+   (val-list (list-of expval?))
+   (body expval?)
+   (cont continuation?))
+  )
+
+
+;; apply-cont : Cont * ExpVal -> FinalAnswer
+(define apply-cont
+  (lambda (cont val)
+    (cases continuation cont
+      (end-cont ()
+                (begin
+                  (eopl:printf "End of computation.~%")
+                  val))
+      (zero1-cont (saved-cont)
+                  (apply-cont saved-cont
+                              (bool-val (zero? (expval->num val)))))
+      (let-exp-cont (var body saved-env saved-cont)
+                    (value-of/k body (extend-env var val saved-env) saved-cont))
+      (if-test-cont (exp2 exp3 saved-env saved-cont)
+                          (if (expval->bool val)
+                              (value-of/k exp2 saved-env saved-cont)
+                              (value-of/k exp3 saved-env saved-cont)))
+      (rator-cont (rands env cont) ;; 好吧，又要开始啦！
+                  (begin
+                    (print "rands -> " rands)
+                    (value-of/k (last-element rands) env ;; 先计算最后一个exp的值
+                                (rand1-cont (except-last-element rands) env cont val '())))) ;; val代表的是proc-val
+      (rand1-cont (rands env cont body acc)
+                  (begin
+                    (print "acc -> " acc)
+                    (print "acc -> " body)
+                    (print "rands -> " rands)
+                    (if (null? (cdr rands)) (value-of/k (last-element rands) env
+                                                        (rand-cont (cons val acc) body cont))
+                        (value-of/k (last-element rands) env
+                                    (rand1-cont (except-last-element rands)
+                                                env cont body (cons val acc))))))
+                  
+      (rand-cont (val-list body cont)
+                 (let ((proc1 (expval->proc body)))
+                   (apply-procedure/k proc1 (cons val val-list) cont)))
+      
+      (diff1-cont (exp2 env cont)
+                  (value-of/k exp2 env
+                              (diff2-cont val cont)))
+      (diff2-cont (val1 cont)
+                  (let ((num1 (expval->num val1))
+                        (num2 (expval->num val)))
+                    (apply-cont cont
+                                (num-val (- num1 num2)))))
+                          
+                          )))
+
+  ;; 可不可以考虑相互递归呢?
+(define (last-element a-list)
+  (if (null? (cdr a-list)) (car a-list)
+      (last-element (cdr a-list))))
+
+(define (except-last-element a-list)
+  (if (null? (cdr a-list)) '()
+      (cons (car a-list) (except-last-element (cdr a-list)))))
+
+
+;; value-of-program : Program -> FinalAnswer
+(define value-of-program
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (exp1)
+                 (value-of/k exp1 (init-env) (end-cont))))))
+
+;; value-of/k : Exp * Env * Cont -> FinalAnswer
+(define value-of/k
+  (lambda (exp env cont)
+    (cases expression exp
+      (const-exp (num) (apply-cont cont (num-val num)))
+      (var-exp (var) (apply-cont cont (apply-env env var)))
+      (proc-exp (var-list body)
+                (apply-cont cont
+                            (proc-val (procedure var-list body env))))
+      (letrec-exp (p-name b-var p-body letrec-body)
+                  (value-of/k letrec-body
+                              (extend-env-rec p-name b-var p-body env)
+                              cont))
+
+      (zero?-exp (exp1)
+                 (value-of/k exp1 env
+                             (zero1-cont cont)))
+      (if-exp (exp1 exp2 exp3)
+              (value-of/k exp1 env
+                          (if-test-cont exp2 exp3 env cont)))
+      (let-exp (var exp1 body)
+                 (value-of/k exp1 env
+                             (let-exp-cont var body env cont)))
+      (diff-exp (exp1 exp2)
+                 (value-of/k exp1 env
+                             (diff1-cont exp2 env cont)))
+      (call-exp (rator rands)
+                  (value-of/k rator env
+                              (rator-cont rands env cont)))
+      )))
+
+;; apply-procedure/k : Proc * ExpVal * Cont -> FinalAnswer
+(define apply-procedure/k
+  (lambda (proc1 val-list cont)
+    (cases proc proc1
+      (procedure (var-list body saved-env)
+                 (value-of/k body
+                             (extend-env-list var-list val-list saved-env)
+                             cont)))))
+
+(define (extend-env-list var-list val-list env)
+  (if (null? var-list) env
+      (extend-env-list (cdr var-list)
+                       (cdr val-list)
+                       (extend-env (car var-list) (car val-list) env))))
+;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program (expression) a-program)
+    (expression (number) const-exp)
+    (expression ("-" "(" expression "," expression ")") diff-exp)
+    (expression ("zero?" "(" expression ")") zero?-exp)
+    (expression ("if" expression "then" expression "else" expression) if-exp)
+    (expression (identifier) var-exp)
+    (expression ("let" identifier "=" expression "in" expression) let-exp)
+    (expression ("proc" "(" (separated-list identifier ",") ")" expression) proc-exp)
+    (expression ("(" expression (arbno expression) ")") call-exp)
+    (expression ("letrec" identifier "(" identifier ")" "=" expression "in" expression) letrec-exp)
+
+    ))
+
+;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+(define-datatype expval expval?
+  (num-val
+   (value number?))
+  (bool-val
+   (boolean boolean?))
+  (proc-val
+   (proc proc?)))
+
+;;; extractors
+
+(define expval->num
+  (lambda (v)
+    (cases expval v
+           (num-val (num) num)
+           (else (expval-extractor-error 'num v)))))
+
+(define expval->bool
+  (lambda (v)
+    (cases expval v
+           (bool-val (bool) bool)
+           (else (expval-extractor-error 'bool v)))))
+
+(define expval->proc
+  (lambda (v)
+    (cases expval v
+           (proc-val (proc) proc)
+           (else (expval-extractor-error 'proc v)))))
+
+(define expval-extractor-error
+  (lambda (variant value)
+    (eopl:error 'expval-extractors "Looking for a ~s, found ~s"
+                variant value)))
+
+;;;;;;;;;;;;;;;; procedures ;;;;;;;;;;;;;;;;
+(define-datatype proc proc?
+  (procedure
+   (bvar (list-of symbol?))
+   (body expression?)
+   (env environment?)))
+
+;;;;;;;;;;;;;;;; environment structures ;;;;;;;;;;;;;;;;
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+   (bvar symbol?)
+   (bval expval?)
+   (saved-env environment?))
+  (extend-env-rec
+   (p-name symbol?)
+   (b-var symbol?)
+   (p-body expression?)
+   (saved-env environment?)))
+
+(define init-env
+  (lambda ()
+    (extend-env
+     'i (num-val 1)
+     (extend-env
+      'v (num-val 5)
+      (extend-env
+       'x (num-val 10)
+       (empty-env))))))
+
+;;;;;;;;;;;;;;;; environment constructors and observers ;;;;;;;;;;;;;;;;
+(define apply-env
+  (lambda (env search-sym)
+    (cases environment env
+           (empty-env ()
+                      (eopl:error 'apply-env "No binding for ~s" search-sym))
+           (extend-env (var val saved-env)
+                       (if (eqv? search-sym var)
+                           val
+                           (apply-env saved-env search-sym)))
+           (extend-env-rec (p-name b-var p-body saved-env)
+                           (if (eqv? search-sym p-name)
+                               (proc-val (procedure b-var p-body env))
+                               (apply-env saved-env search-sym))))))
+
+(define run
+  (lambda (string)
+    (value-of-program (scan&parse string))))
+
+;; test code
+(run "let p = proc(x, y) -(x, y) in (p 1 2)")
+
+
